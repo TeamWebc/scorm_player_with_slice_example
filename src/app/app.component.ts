d69f83c5e12e14ba8635f24af278a5e60bb83116
Matan Avraham <matan@MacBook-Pro.local>
2021-09-09 15:27:10 +0300

    ‘matan’

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { NgScormPlayerService } from 'ng-scorm-player'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public player:NgScormPlayerService){
+
+  }
+
+  open = true;
+
+
   title = 'scorm-raper';
+// player = this._NgScormPlayerService;
+  urlDirOfSco = '../assets/demo/';
+  urlLaunchPage = ''
+  scormResult:any;
+
+  ngOnInit() {
+    this.player.initializeEvent.subscribe(val => { console.log('initializeEvent:', val); });
+    this.player.setValueEvent.subscribe(val => { console.log('setValueEvent:', val); }); 
+    this.player.getValueEvent.subscribe(val => { console.log('getValueEvent:', val); });
+    this.player.finishEvent.subscribe(val => { console.log('finishEvent:', val); }); 
+    this.player.commitEvent.subscribe(val => { console.log('commitEvent:', val); });
+ 
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { NgScormPlayerModule } from 'ng-scorm-player';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgScormPlayerModule.forChild({debug : true})
   ],
   providers: [],
   bootstrap: [AppComponent]
